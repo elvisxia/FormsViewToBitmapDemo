54c67db9f4313ddeecac1257d909c9f7f34e4280
Elvis Xia (Shang Hai Wei Chuang Ruan Jian) <xux@microsoft.com>
2018-07-06 15:22:34 +0800

    Add project files.

---
+++ FormsViewToBitmapDemo/FormsViewToBitmapDemo.Android/MyViewRenderer.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using FormsViewToBitmapDemo.Droid;
+using Xamarin.Forms;
+using FormsViewToBitmapDemo;
+using Xamarin.Forms.Platform.Android;
+using Android.Graphics;
+using Java.IO;
+using System.IO;
+using Android.Content.PM;
+using Android.Support.V4.App;
+
+[assembly: ExportRenderer(typeof(MyView), typeof(MyViewRenderer))]
+namespace FormsViewToBitmapDemo.Droid
+{
+    public class MyViewRenderer : ViewRenderer<MyView, Android.Views.View>
+    {
+        public MyViewRenderer(Context c) : base(c)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<MyView> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                //register the event
+                e.NewElement.OnDrawBitmap += NewElement_OnDrawBitmap;
+            }
+        }
+
+        private void NewElement_OnDrawBitmap(object sender, EventArgs e)
+        {
+            if (this.ViewGroup != null)
+            {
+                //get the subview
+                Android.Views.View subView = ViewGroup.GetChildAt(0);
+                int width = subView.Width;
+                int height = subView.Height;
+
+                //create and draw the bitmap
+                Bitmap b = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
+                Canvas c = new Canvas(b);
+                ViewGroup.Draw(c);
+
+                //save the bitmap to file
+                SaveBitmapToFile(b);
+            }
+        }
+
+        void SaveBitmapToFile(Bitmap bm)
+        {
+            //before save, we need to request permission
+            RequestStoragePermission();
+
+            var sdCardPath = Android.OS.Environment.ExternalStorageDirectory.AbsolutePath;
+            var filePath = System.IO.Path.Combine(sdCardPath, "test.png");
+            var stream = new FileStream(filePath, FileMode.OpenOrCreate);
+            bm.Compress(Bitmap.CompressFormat.Png, 100, stream);
+            stream.Close();
+        }
+
+        void RequestStoragePermission()
+        {
+            var activity = Forms.Context as MainActivity;
+            if (activity.CheckSelfPermission(Android.Manifest.Permission.WriteExternalStorage) == Permission.Granted)
+            {
+                return;
+            }
+            else
+            {
+                ActivityCompat.RequestPermissions(activity, new string[] { Android.Manifest.Permission.WriteExternalStorage }, 1);
+            }
+        }
+    }
+}
---
+++ FormsViewToBitmapDemo/FormsViewToBitmapDemo/MainPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsViewToBitmapDemo
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            Grid grid = new Grid();
+
+            for (int i = 0; i < 30; i++)
+            {
+                RowDefinition row = new RowDefinition
+                {
+                    Height = new GridLength(30,GridUnitType.Absolute)
+                };
+                ColumnDefinition col = new ColumnDefinition
+                {
+                    Width = new GridLength(30,GridUnitType.Absolute)
+                };
+                grid.RowDefinitions.Add(row);
+                grid.ColumnDefinitions.Add(col);
+            }
+
+            for (int row = 0; row < 30; row++)
+            {
+                for (int col = 0; col < 30; col++)
+                {
+                    Label label = new Label
+                    {
+                        Text = "Row:" + row + "; Col:" + col
+                    };
+                    if (col == 29 || row == 29)
+                    {
+                        label.TextColor = Color.Red;
+                    } else
+                    {
+                        label.TextColor = Color.Black;
+                    }
+                    grid.Children.Add(label, col,row);
+                }
+            }
+            root.Children.Add(grid);
+            btnClick.Clicked += BtnClick_Clicked;
+        }
+
+        private void BtnClick_Clicked(object sender, EventArgs e)
+        {
+            root.OnDrawBitmap.Invoke(root, new EventArgs());
+        }
+    }
+}
